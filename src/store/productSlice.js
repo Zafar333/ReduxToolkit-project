1a0fee0f946e442969b50cd2f17c2cca81f1cad2
Arslan Zafar <arslanz499@gmail.com>
2023-06-02 21:58:27 +0500

    implemented

--- src/store/productSlice.js
+++ src/store/productSlice.js
@@ -1,5 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
-const initialState = { data: [] };
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+const initialState = { data: [], status: "idle" };
 
 const productSlice = createSlice({
   name: "Products",
@@ -10,14 +10,30 @@ const productSlice = createSlice({
       state.data = action.payload;
     },
   },
+  extraReducers: (builder) => {
+    builder.addCase(getProducts.fulfilled, (state, action) => {
+      state.data = action.payload;
+    });
+  },
 });
 export const { fetchProducts } = productSlice.actions;
 export default productSlice.reducer;
 
-export function getProducts() {
-  return async function getProductsThunk(dispatch, getstate) {
-    const data = await fetch("https://fakestoreapi.com/products");
-    const result = await data.json();
-    dispatch(fetchProducts(result));
-  };
-}
+// it is first function technique to call for api using thunk middleware
+
+export const getProducts = createAsyncThunk("product/get", async () => {
+  const data = await fetch("https://fakestoreapi.com/products");
+  const result = await data.json();
+  return result;
+});
+
+// its a function for api request and its choice for you which one function is used for request
+// using thunk function or simple its your it is a simple function without using thunk
+
+// export function getProducts() {
+//   return async function getProductsThunk(dispatch, getstate) {
+//     const data = await fetch("https://fakestoreapi.com/products");
+//     const result = await data.json();
+//     dispatch(fetchProducts(result));
+//   };
+// }
